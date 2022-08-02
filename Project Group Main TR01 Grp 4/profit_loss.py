a2f159575ded3e16644c35d5eaaf6c21e83c724d
yasminezulkifli <110328625+yasminezulkifli@users.noreply.github.com>
2022-08-02 22:42:54 +0800

    created and defiend tha profit and lost function, yall please double check for me, not too sure bout line 12

---
+++ Project Group Main TR01 Grp 4/profit_loss.py
@@ -0,0 +1,22 @@
+def calculate_profit_and_lost(pl_dict_in_list): 
+    current_netprofit_value = 0 #sublist
+    pl_dip_list = list() #creating list object 
+
+    for variable in pl_dict_in_list:
+        if float(variable['Net Porfit']) >= float(current_netprofit_value):
+            current_netprofit_value = float(variable['Net Profit'])
+
+        else: 
+            diff_btw_days = float(current_netprofit_value) - float(variable['Net Profit'])
+            # computing the diff in the net profit btw each day 
+            pl_dip_list.append({variable['"Day"']: diff_btw_days})
+            current_netprofit_value = float(variable['Net Profit'])
+
+    print("PROFIT LOST DIP RESULTS; [USD]:")
+    for result in pl_dip_list:
+        for key, value in result.items():
+            print("DAY {0} , SALES {1}".format(key,value))
+
+    return pl_dip_list
+
+
