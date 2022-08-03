1ea1ce35372f3f32516efab1beb76ab12688cb3f
gracietann <110330278+gracietann@users.noreply.github.com>
2022-08-03 23:06:06 +0800

    updated cash_on_hand.py

---
+++ Project Group Main TR01 Grp 4/cash_on_hand.py
@@ -0,0 +1,19 @@
+def calculate_dip(coh_dict_in_list):
+    current_coh_value = 0
+    coh_dip_list = list()
+
+    for item in coh_dict_in_list:
+        if float(item['Cash On Hand']) >= float(current_coh_value):
+            current_coh_value = float(item['Cash On Hand'])
+        else:
+            diff_between_days = float(current_coh_value) - float(item['Cash On Hand'])
+            coh_dip_list.append({item['ï»¿"Day"']: diff_between_days})
+            current_coh_value = float(item['Cash On Hand'])
+
+    print("\n-> COH DIP RESULTS: [USD]:")
+    #new line 
+    for result in coh_dip_list:
+        for key, value in result.items():
+            print("DAY: {0} | AMT: {1}".format(key, value))
+
+    return coh_dip_list
