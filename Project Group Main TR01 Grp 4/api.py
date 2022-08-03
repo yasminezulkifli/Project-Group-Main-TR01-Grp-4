a2b9fb599a647beacc08d84578f278077a4306fc
yasminezulkifli <110328625+yasminezulkifli@users.noreply.github.com>
2022-08-04 00:05:03 +0800

    fixed the error

--- Project Group Main TR01 Grp 4/api.py
+++ Project Group Main TR01 Grp 4/api.py
@@ -1,25 +1,24 @@
 from xml.dom import UserDataHandler
 from forex_python.converter import CurrencyRates 
-currency = CurrencyRates()
-print(currency.get_rates('USD'))
 
 import requests
 import json 
 api_key = "VW2YB37U2F0K0NDE"
 url =f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
 
+function = "CURRENCY_EXCHANGE_RATE"
+from_currency = "USD"
+to_currency = "SGD"
+
 def api_function():
-    function = "CURRENCY_EXCHANGE_RATE"
-    from_currency = "USD"
-    to_currency = "SGD"
     response = requests.get(url)
-    exchange_rate = -1
+    data = response.json()
     
      #to check if the response is good to go
     if response.status_code == 200: 
         data = response.json()
 
-        for key, value in data['Realtime Currency Excahne Rate'].items():
+        for key, value in data['Realtime Currency Exchange Rate'].items():
             if "Exchange Rate" in key: 
                 exchange_rate = value 
 
