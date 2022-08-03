55f7502e0b765411f8f630c3bcfa29c224f919ad
yasminezulkifli <110328625+yasminezulkifli@users.noreply.github.com>
2022-08-04 00:13:28 +0800

    deleted line 25 - print, use return instead

--- Project Group Main TR01 Grp 4/profit_loss.py
+++ Project Group Main TR01 Grp 4/profit_loss.py
@@ -1,6 +1,7 @@
 def calculate_profit_and_lost(pl_dict_in_list): 
     current_netprofit_value = 0 
-    pl_dip_list = list() #creating list object 
+    pl_dip_list = list() 
+    # creating list object 
 
     for variable in pl_dict_in_list:
         if float(variable['Net Porfit']) >= float(current_netprofit_value):
@@ -9,14 +10,15 @@ def calculate_profit_and_lost(pl_dict_in_list):
         else: 
             diff_btw_days = float(current_netprofit_value) - float(variable['Net Profit'])
             # computing the diff in the net profit btw each day 
-            pl_dip_list.append({variable['"Day"']: diff_btw_days}) #from nested list
+
+            pl_dip_list.append({variable['"Day"']: diff_btw_days})
+            # nested list
             current_netprofit_value = float(variable['Net Profit'])
 
     print("PROFIT LOST DIP RESULTS; [USD]:")
+    # FOR MAIN.PY
     for result in pl_dip_list:
         for key, value in result.items():
             print("DAY {0} , SALES {1}".format(key,value))
 
     return pl_dip_list
-
-print (pl_dict_in_list)
