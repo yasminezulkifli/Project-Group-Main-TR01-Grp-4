7257b58196bc0f37b1e96d90118be7b1fade856a
yasminezulkifli <110328625+yasminezulkifli@users.noreply.github.com>
2022-08-04 10:42:04 +0800

    documentation for p and l

--- Project Group Main TR01 Grp 4/profit_loss.py
+++ Project Group Main TR01 Grp 4/profit_loss.py
@@ -1,5 +1,5 @@
 def calculate_profit_and_lost(pl_dict_in_list): 
-    current_netprofit_value = 0 
+    current_netprofit_value = 0 #why 0? "Python 0 is an indicator of the format method that you need to be replaced by the format's first (index zero) parameter."
     pl_dip_list = list() 
     # creating list object 
 
@@ -11,14 +11,14 @@ def calculate_profit_and_lost(pl_dict_in_list):
             diff_btw_days = float(current_netprofit_value) - float(variable['Net Profit'])
             # computing the diff in the net profit btw each day 
 
-            pl_dip_list.append({variable['"Day"']: diff_btw_days})
-            # nested list
+            pl_dip_list.append({variable['"Day"']: diff_btw_days}) 
+            
             current_netprofit_value = float(variable['Net Profit'])
 
     print("PROFIT LOST DIP RESULTS; [USD]:")
-    # FOR MAIN.PY
+    # IF THERE IS A DIFF 
     for result in pl_dip_list:
         for key, value in result.items():
-            print("DAY {0} , SALES {1}".format(key,value))
+            print("DAY {0} , AMT {1}".format(key,value))
 
     return pl_dip_list
