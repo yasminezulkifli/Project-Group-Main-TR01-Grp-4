4db08165bdfd7b9bd82bc6e0fb9feb63671529e8
gracietann <110330278+gracietann@users.noreply.github.com>
2022-08-04 10:56:25 +0800

    Added documentation for ash_on_hand.py

--- Project Group Main TR01 Grp 4/cash_on_hand.py
+++ Project Group Main TR01 Grp 4/cash_on_hand.py
@@ -1,17 +1,21 @@
 def calculate_dip(coh_dict_in_list):
+    #declaring the variable to store monthly average 
     current_coh_value = 0
     coh_dip_list = list()
-
+    #nested list
+    
     for item in coh_dict_in_list:
+        #for loop used to iterate through nested list. items is used as temporary holder for nested list
         if float(item['Cash On Hand']) >= float(current_coh_value):
             current_coh_value = float(item['Cash On Hand'])
         else:
             diff_between_days = float(current_coh_value) - float(item['Cash On Hand'])
+            #obtaining difference between day using formula of subtracting the Cash on Hand on that day from the current Cash on hand. 
             coh_dip_list.append({item["Day"]: diff_between_days})
             current_coh_value = float(item['Cash On Hand'])
 
     print("COH DIP RESULTS: [USD]:")
-    #new line 
+   
     for result in coh_dip_list:
         for key, value in result.items():
             print("DAY: {0} | AMT: {1}".format(key, value))
